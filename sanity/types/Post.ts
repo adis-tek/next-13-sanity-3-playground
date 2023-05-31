f1169718fd6b9e19b13dd2118e52bd9ef281df6b
adis-tek <67308354+adis-tek@users.noreply.github.com>
2023-05-31 02:25:50 -0500

    sanity text and image ready

---
+++ sanity/types/Post.ts
@@ -0,0 +1,25 @@
+// groq`
+//     *[_type == "post"]{
+//       title,
+//       slug,
+//       mainImage{
+//         asset->{
+//           _id,
+//           url
+//         },
+//         alt
+//       }
+//     }`;
+
+import { PortableTextBlock } from "sanity";
+
+export interface Post {
+  _id: string;
+  title: string;
+  slug: {
+    current: string;
+  };
+  imageUrl: string;
+  alt: string;
+  body: PortableTextBlock[];
+}
